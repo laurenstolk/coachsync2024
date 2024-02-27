58f2f93602a4b447c84a53abb5263e4915f644c4
Lauren Stolk <laurenstolk@outlook.com>
2024-02-27 10:45:20 -0700

    prettier

--- client/src/getTeamLogoUrl.js
+++ client/src/getTeamLogoUrl.js
@@ -1,7 +1,9 @@
 import { supabase } from "./supabaseClient";
 
 export async function getTeamLogoURL(file_path) {
-  const { data, error } = await supabase.storage.from("images/team_logos").createSignedUrl(`${file_path}`, 60);
+  const { data, error } = await supabase.storage
+    .from("images/team_logos")
+    .createSignedUrl(`${file_path}`, 60);
 
   return data.signedUrl;
 }
