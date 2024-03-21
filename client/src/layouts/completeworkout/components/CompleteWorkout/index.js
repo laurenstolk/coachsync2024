720f921d8d90c9db2a6cd22cad93b6cb4210a911
littlej77 <jordantempest@gmail.com>
2024-03-21 13:16:07 -0600

    Update index.js
    idek

--- client/src/layouts/completeworkout/components/CompleteWorkout/index.js
+++ client/src/layouts/completeworkout/components/CompleteWorkout/index.js
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 
-import { FormControl, FormControlLabel, InputLabel } from "@mui/material";
+import { Divider, FormControl, FormControlLabel, InputLabel } from "@mui/material";
 
 import React, { useEffect, useState, useMemo } from "react";
 
@@ -21,10 +21,14 @@ import { fetchUserProfile } from "../../../../fetchUserProfile";
 import Checkbox from "@mui/material/Checkbox";
 
 async function getCustomizedExercise(assignmentData) {
+  console.log("Assignment Data:", assignmentData); // Log assignmentData to ensure it's correct
+
   const { data: customizedExercisesData } = await supabase
     .from("customized_exercise")
     .select("*, id")
     .eq("workout_id", assignmentData.workout_id);
+
+    console.log("Customized Exercises Data:", customizedExercisesData); // Log customizedExercisesData to see what's retrieved
 
   // Fetch exercise data separately
   const { data: exerciseData } = await supabase.from("exercise").select("*");
@@ -43,23 +47,28 @@ async function getCustomizedExercise(assignmentData) {
       exercise_id: exerciseInfo.id, // Add the exercise_id from exerciseInfo
       ...exerciseInfo,
     };
+    console.log("Customized Exercises with Exercise Info:", customizedExercisesWithExerciseInfo); // Log the merged data
+
   });
 
   return customizedExercisesWithExerciseInfo;
 }
-
-async function getAssignment(profile) {
+async function getAssignments(profile) {
   const today = new Date();
   today.setHours(0, 0, 0, 0);
   console.log("today: ", today.toISOString().split("T")[0]);
-  const { data: assignmentData } = await supabase
+  const { data: assignmentData, error } = await supabase
     .from("assignment")
     .select("*")
     .eq("player_id", profile.id)
-    .eq("date", today.toISOString().split("T")[0])
-    .single();
-  console.log("assignments: ", assignmentData);
+    .eq("date", today.toISOString().split("T")[0]);
+  
+  if (error) {
+    console.error("Error fetching assignments:", error);
+    return null;
+  }
 
+  console.log("assignments: ", assignmentData);
   return assignmentData;
 }
 
@@ -69,83 +78,193 @@ async function getWorkout(assignmentData) {
     .select("*")
     .eq("id", assignmentData.workout_id)
     .single();
-
+  console.log("assignment workoutid:", assignmentData.workout_id)
+  
   return workoutData;
 }
 
+
 function CompleteWorkout() {
   const navigate = useNavigate();
-  const [assignment, setAssignment] = useState(null);
+  const [assignment, setAssignment] = useState([]);
   const [workout, setWorkout] = useState(null);
   const [customizedExercises, setCustomizedExercises] = useState([]);
   const [completedExercises, setCompletedExercises] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+   // Add a useMemo hook to check if any exercises are completed
+  const anyExercisesCompleted = useMemo(() => {
+    return completedExercises.length > 0;
+  }, [completedExercises]);
+
+  console.log('Customized Exercises Length:', customizedExercises.length);
+  console.log('Completed Exercises Length:', completedExercises.length);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const profileData = await fetchUserProfile();
+  //       const assignmentData = await getAssignments(profileData);
+
+  //       if (!assignmentData || assignmentData.length === 0) {
+  //         setIsLoading(false);
+  //         return;
+  //       }
+  //       const assignmentsPromises = assignmentData.map(async (assignment) => {
+  //         const customizedExercisesData = await getCustomizedExercise(assignment);
+  //         const workoutData = await getWorkout(assignment);
+  //         return { assignment, customizedExercisesData, workoutData };
+  //       });
+  //       // Resolve all promises
+  //       const resolvedAssignments = await Promise.all(assignmentsPromises);
+  //       // Update state with the fetched data
+  //       setAssignment(resolvedAssignments);
+  //       setIsLoading(false);
+
+  //       } catch (error) {
+  //       console.error("Error fetching data");
+  //       setIsLoading(false); // Ensure loading is set to false even in case of errors
+  //       }
+  //     };
+  //     fetchData();
+  //   }, []);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const profileData = await fetchUserProfile();
-        const assignmentData = await getAssignment(profileData);
-
-        if (!assignmentData) {
+        const assignmentData = await getAssignments(profileData);
+  
+        if (!assignmentData || assignmentData.length === 0) {
           setIsLoading(false);
           return;
         }
-
-        setAssignment(assignmentData);
-        const customizedExercisesData = await getCustomizedExercise(assignmentData);
-        setCustomizedExercises(customizedExercisesData);
-        const workoutData = await getWorkout(assignmentData);
-        setWorkout(workoutData);
+  
+        const assignmentsPromises = assignmentData.map(async (assignment) => {
+          const customizedExercisesData = await getCustomizedExercise(assignment);
+          const workoutData = await getWorkout(assignment);
+          return { assignment, customizedExercisesData, workoutData };
+        });
+  
+        // Resolve all promises
+        const resolvedAssignments = await Promise.all(assignmentsPromises);
+  
+        // Update state with the fetched data
+        setAssignment(resolvedAssignments);
         setIsLoading(false);
+  
+        // Log the lengths after setting the state
+        console.log('Customized Exercises Length:', resolvedAssignments[0].customizedExercisesData.length);
+        console.log('Completed Exercises Length:', completedExercises.length);
+  
       } catch (error) {
         console.error("Error fetching data");
+        setIsLoading(false); // Ensure loading is set to false even in case of errors
       }
     };
+  
     fetchData();
   }, []);
 
+  //       setAssignment(assignmentData);
+  //       const customizedExercisesData = await getCustomizedExercise(assignmentData);
+  //       setCustomizedExercises(customizedExercisesData);
+  //       const workoutData = await getWorkout(assignmentData);
+  //       setWorkout(workoutData);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.error("Error fetching data");
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
   // Add a useMemo hook to calculate whether all exercises are completed
   const allExercisesCompleted = useMemo(() => {
+    console.log("cust ex leng:", customizedExercises.length)
     return customizedExercises.length === completedExercises.length;
   }, [customizedExercises, completedExercises]);
 
   // Function to handle checkbox change for completed exercises
-  // Function to handle checkbox change for completed exercises
+  // const handleExerciseCompletionChange = (exercise) => {
+  //   console.log('Exercise:', exercise);
+  //   setCompletedExercises((prevCompletedExercises) => {
+  //     const isAlreadyCompleted = prevCompletedExercises.some(
+  //       (completedExercise) =>
+  //         completedExercise.customized_exercise_id === exercise.customized_exercise_id
+  //     );
+  //     console.log("already completed", completedExercises)
+
+  //     if (isAlreadyCompleted) {
+  //       console.log('Removing exercise from completed:', exercise);
+  //       // If the exercise is already completed, remove it from completedExercises
+  //       const updatedCompletedExercises = prevCompletedExercises.filter(
+  //         (completedExercise) =>
+  //           completedExercise.customized_exercise_id !== exercise.customized_exercise_id
+  //       );
+  //       return updatedCompletedExercises;
+  //     } else {
+  //       console.log('Adding exercise to completed:', exercise);
+  //       // If the exercise is not completed, add it to completedExercises
+  //       return [...prevCompletedExercises, exercise];
+  //     }
+  //   });
+  // };
   const handleExerciseCompletionChange = (exercise) => {
+    console.log('Exercise:', exercise);
     setCompletedExercises((prevCompletedExercises) => {
       const isAlreadyCompleted = prevCompletedExercises.some(
         (completedExercise) =>
           completedExercise.customized_exercise_id === exercise.customized_exercise_id
       );
-
-      if (isAlreadyCompleted) {
+      console.log("already completed", completedExercises)
+  
+      if (!isAlreadyCompleted) {
+        console.log('Adding exercise to completed:', exercise);
+        // If the exercise is not completed, add it to completedExercises
+        return [...prevCompletedExercises, exercise];
+      } else {
+        console.log('Removing exercise from completed:', exercise);
         // If the exercise is already completed, remove it from completedExercises
         const updatedCompletedExercises = prevCompletedExercises.filter(
           (completedExercise) =>
             completedExercise.customized_exercise_id !== exercise.customized_exercise_id
         );
+        console.log("already completed final", updatedCompletedExercises)
+
         return updatedCompletedExercises;
-      } else {
-        // If the exercise is not completed, add it to completedExercises
-        return [...prevCompletedExercises, exercise];
       }
     });
   };
 
+  // const handleNotesChange = (index, value) => {
+  //   setCustomizedExercises((prevCustomizedExercises) => {
+  //     const updatedExercises = [...prevCustomizedExercises];
+  //     updatedExercises[index].notes = value; // Update the notes for the corresponding exercise
+  //     return updatedExercises;
+  //   });
+  // };
   const handleNotesChange = (index, value) => {
     setCustomizedExercises((prevCustomizedExercises) => {
-      const updatedExercises = [...prevCustomizedExercises];
-      updatedExercises[index].notes = value; // Update the notes for the corresponding exercise
-      return updatedExercises;
+      // Check if the exercise at the given index exists
+      if (prevCustomizedExercises[index]) {
+        const updatedExercises = [...prevCustomizedExercises];
+        updatedExercises[index].notes = value; // Update the notes for the corresponding exercise
+        return updatedExercises;
+      } else {
+        // Handle the case where the exercise at the given index does not exist
+        console.error(`Exercise at index ${index} does not exist`);
+        return prevCustomizedExercises;
+      }
     });
   };
 
   // Function to handle form submission
   const handleSubmit = async () => {
     try {
+      // console.log('Customized Exercises Length:', customizedExercises.length);
+      // console.log('Completed Exercises Length:', completedExercises.length);
       // Check if all exercises are completed
-      const allExercisesCompleted = customizedExercises.length === completedExercises.length;
+      const allExercisesCompleted =  resolvedAssignments[0].customizedExercisesData.length === completedExercises.length;
 
       if (allExercisesCompleted) {
         // If all exercises are completed, update the 'completed' field of the assignment to TRUE
@@ -215,7 +334,7 @@ function CompleteWorkout() {
       <Card id="delete-account">
         <MDBox pt={3} px={2}>
           <MDTypography variant="h4" fontWeight="medium">
-            There is no assigned workout for today.
+            There is no assigned workout(s) for today.
           </MDTypography>
         </MDBox>
         <MDBox pt={1} pb={2} px={2}></MDBox>
@@ -257,92 +376,104 @@ function CompleteWorkout() {
     );
   } else {
     return (
-      <Card id="delete-account">
-        <MDBox pt={3} px={2}>
-          <MDTypography variant="h4" fontWeight="medium">
-            {workout ? workout.workout_name : "Today's Workout"}
-          </MDTypography>
-        </MDBox>
-
-        <MDBox pt={1} pb={2} px={2}>
-          <InputLabel>Exercises</InputLabel>
-          <MDBox
-            component="ul"
-            display="flex"
-            flexDirection="column"
-            p={0}
-            m={0}
-            className="MuiFormControlLabel-root"
-          >
-            {" "}
-            {/* Add the className here */}
-            <MDBox mb={2}>
-              <FormControl>
-                <MDBox display="flex" flexDirection="column">
-                  {customizedExercises.map((customizedExercise, index) => (
-                    <FormControlLabel
-                      key={index}
-                      control={
-                        <Checkbox
-                          checked={completedExercises.includes(customizedExercise)}
-                          onChange={() => handleExerciseCompletionChange(customizedExercise)}
-                          name={customizedExercise.name}
-                        />
-                      }
-                      label={
-                        <div>
-                          <MDTypography variant="body1" fontWeight="medium">
-                            {customizedExercise.name}
-                          </MDTypography>
-                          {(customizedExercise.reps ||
-                            customizedExercise.sets ||
-                            customizedExercise.duration) && (
-                            <MDTypography variant="body2">
-                              {customizedExercise.reps && `Reps: ${customizedExercise.reps}`}
-                              {customizedExercise.reps &&
-                                (customizedExercise.sets || customizedExercise.duration) &&
-                                " | "}
-                              {customizedExercise.sets && `Sets: ${customizedExercise.sets}`}
-                              {customizedExercise.sets && customizedExercise.duration && " | "}
-                              {customizedExercise.duration &&
-                                `Duration: ${customizedExercise.duration}`}
-                            </MDTypography>
-                          )}
-
-                          <MDTypography variant="body2" color="text">
-                            Coach Notes: {customizedExercise.coach_notes}
-                          </MDTypography>
-                          <TextField
-                            label="Notes"
-                            variant="outlined"
-                            size="small"
-                            fullWidth
-                            multiline
-                            rows={1}
-                            placeholder="Enter notes..."
-                            onChange={(event) => handleNotesChange(index, event.target.value)}
-                          />
-                        </div>
-                      }
-                    />
-                  ))}
-                </MDBox>
-              </FormControl>
-            </MDBox>
+    <div>
+      {assignment.map((assignmentItem, assignmentIndex) => (
+        <Card key={`assignment-${assignmentIndex}`}>
+          <MDBox pt={3} px={2}>
+            <MDTypography variant="h4" fontWeight="medium">
+              {assignmentItem.workoutData ? assignmentItem.workoutData.workout_name : "Unknown"}
+            </MDTypography>
+            {assignmentItem.assignment.completed && (
+              <div>
+                <MDTypography variant="body1">You have already completed this workout</MDTypography>
+                <Divider style={{ backgroundColor: 'black' }} sx={{ my: 2, height: 4 }}></Divider>
+              </div>
+            )}
           </MDBox>
-        </MDBox>
 
-        {/* Submit button */}
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-          disabled={!allExercisesCompleted} // Disable the button if not all exercises are completed
-        >
-          Submit
-        </Button>
-      </Card>
+          {!assignmentItem.assignment.completed && ( // Conditionally render if assignment is not completed
+            <MDBox pt={1} pb={2} px={2}>
+              <InputLabel>Exercises</InputLabel>
+              <MDBox
+                component="ul"
+                display="flex"
+                flexDirection="column"
+                p={0}
+                m={0}
+                className="MuiFormControlLabel-root"
+              >
+                {assignmentItem.customizedExercisesData.map((customizedExercise, exerciseIndex) => (
+                  <MDBox mb={2} key={`exercise-${exerciseIndex}`}>
+                    <FormControl>
+                      <MDBox display="flex" flexDirection="column">
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={completedExercises.includes(customizedExercise)}
+                              onChange={() => handleExerciseCompletionChange(customizedExercise)}
+                              name={customizedExercise.name}
+                            />
+                          }
+                          label={
+                            <div>
+                              <MDTypography variant="body1" fontWeight="medium">
+                                {customizedExercise.name}
+                              </MDTypography>
+                              {(customizedExercise.reps ||
+                                customizedExercise.sets ||
+                                customizedExercise.duration) && (
+                                <MDTypography variant="body2">
+                                  {customizedExercise.reps && `Reps: ${customizedExercise.reps}`}
+                                  {customizedExercise.reps &&
+                                    (customizedExercise.sets || customizedExercise.duration) &&
+                                    " | "}
+                                  {customizedExercise.sets && `Sets: ${customizedExercise.sets}`}
+                                  {customizedExercise.sets && customizedExercise.duration && " | "}
+                                  {customizedExercise.duration &&
+                                    `Duration: ${customizedExercise.duration}`}
+                                </MDTypography>
+                              )}
+
+                              <MDTypography variant="body2" color="text">
+                                Coach Notes: {customizedExercise.coach_notes}
+                              </MDTypography>
+                              <TextField
+                                label="Notes"
+                                variant="outlined"
+                                size="small"
+                                fullWidth
+                                multiline
+                                rows={1}
+                                placeholder="Enter notes..."
+                                onChange={(event) => handleNotesChange(exerciseIndex, event.target.value)}
+                              />
+                            </div>
+                          }
+                        />
+                      </MDBox>
+                    </FormControl>
+                  </MDBox>
+                ))}
+              </MDBox>
+            </MDBox>
+          )}
+
+          {!assignmentItem.assignment.completed && ( // Conditionally render if assignment is not completed
+            // Submit button
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+              style={{ color: 'white' }} 
+              disabled={!allExercisesCompleted} // Disable the button if not all exercises are completed
+            >
+              Submit
+            </Button>
+          )}
+        </Card>
+      ))}
+    </div>
+
     );
-  }
-}
+}}
 export default CompleteWorkout;
