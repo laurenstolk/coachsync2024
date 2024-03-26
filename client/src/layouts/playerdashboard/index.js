6d518b6d7a96513c944fcc6f0f9a173ae4d8797c
bkimb12 <braedonk@byu.edu>
2024-03-26 16:10:00 -0600

    fix check in box and remove comments

--- client/src/layouts/playerdashboard/index.js
+++ client/src/layouts/playerdashboard/index.js
@@ -1,17 +1,11 @@
-/**
-=========================================================
+/**=========================================================
 * Material Dashboard 2 React - v2.2.0
 =========================================================
-
 * Product Page: https://www.creative-tim.com/product/material-dashboard-react
 * Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
 Coded by www.creative-tim.com
-
  =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.*/
 
 // @mui material components
 import Grid from "@mui/material/Grid";
@@ -42,7 +36,6 @@ export default function PlayerDashboard() {
   const [workoutCompleted, setWorkoutCompleted] = useState(false);
   const [checkinCompleted, setCheckinCompleted] = useState(false);
   const [checkinFrequency, setCheckinFrequency] = useState("");
-  const [nextCheckinDay, setNextCheckinDay] = useState("");
   const [assignedWorkout, setAssignedWorkout] = useState(null);
   const [currentDate, setCurrentDate] = useState("");
   const [user, setUser] = useState(null);
@@ -123,36 +116,6 @@ export default function PlayerDashboard() {
 
     fetchTeamCheckinFrequency();
 
-    // Calculate and set the next check-in day
-    const getNextCheckinDay = () => {
-      const currentDayOfWeek = today.getDay();
-      const daysOfWeek = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-
-      // Extract individual digits from the checkin_frequency
-      const frequencyDigits = checkinFrequency.split("").map(Number);
-
-      // Find the next available check-in day
-      for (let i = 1; i <= 7; i++) {
-        const nextDayIndex = (currentDayOfWeek + i) % 7;
-        if (frequencyDigits.includes(nextDayIndex + 1)) {
-          return daysOfWeek[nextDayIndex];
-        }
-      }
-
-      return null; // Return null if no scheduled check-in days found
-    };
-
-    const nextCheckinDay = getNextCheckinDay();
-    setNextCheckinDay(nextCheckinDay);
-
     // ... (other code)
   }, [user]); // removed this (user, today, checkinFrequency) from the array -- may have been constantly refreshing everything
 
@@ -164,6 +127,34 @@ export default function PlayerDashboard() {
   };
 
   const checkinRequired = isCheckinRequired();
+
+  // Calculate and set the next check-in day
+  const getNextCheckinDay = () => {
+    const currentDayOfWeek = today.getDay();
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    // Extract individual digits from the checkin_frequency
+    const frequencyDigits = checkinFrequency.split("").map(Number);
+  
+    // Find the next available check-in day
+    for (let i = 1; i <= 7; i++) {
+      const nextDayIndex = (currentDayOfWeek + i) % 7;
+      if (frequencyDigits.includes(nextDayIndex + 1)) {
+        return daysOfWeek[nextDayIndex];
+      }
+    }
+    return null; // Return null if no scheduled check-in days found
+  };
+  
+  const nextCheckinDay = getNextCheckinDay();
 
   //function to display next workout day
   const getNextWorkoutDay = async () => {
@@ -281,52 +272,6 @@ export default function PlayerDashboard() {
     fontSize = 25; // Default font size if assignedWorkout is null or undefined
   }
 
-  // //working on the line chart
-  // const currentDay = new Date(); // Get today's date
-  // const labels = []; // Initialize an array to hold the labels
-
-
-  // // Loop through the past 7 days and generate labels
-  // for (let i = 6; i >= 0; i--) {
-  //   const date = new Date(currentDay); // Create a new date object for each day
-  //   date.setDate(today.getDate() - i); // Subtract i days from today's date
-  //   labels.push(date.toLocaleDateString("en-US", { month: "short", day: "numeric" })); // Format the date and push it to the labels array
-  // };
-
-
-  //checkin chart
-
-  //const [checkinData, setCheckinData] = useState(null); // State variable for storing checkin data
-
-  // useEffect(() => {
-  //   // Fetch checkin data
-  //   const fetchCheckinData = async () => {
-  //     try {
-  //       const { data: checkinData, error: checkinError } = await supabase
-  //         .from("checkin")
-  //         .select("date, wellness_id, value")
-  //         .eq("player_id", user.id)
-  //         .gte("date", new Date(today.getTime() - 6 * 24 * 60 * 60 * 1000).toISOString()) // Fetch data for the past 7 days
-  //         .order("date");
-
-  //       if (checkinError) {
-  //         throw checkinError;
-  //       }
-
-  //       setCheckinData(checkinData);
-  //     } catch (error) {
-  //       console.error("Error fetching checkin data:", error.message);
-  //     }
-  //   };
-
-  //   if (user && user.id) {
-  //     fetchCheckinData();
-  //   }
-  // }, []);
-
-  // console.log("This is the checkin data ",checkinData)
-  // //end checkin chart
-
   const [chartData, setChartData] = useState(null);
 
   useEffect(() => {
@@ -387,8 +332,6 @@ export default function PlayerDashboard() {
             formattedData.datasets[index].data.push(entry ? entry.value : 0);
           }
         });
-
-        console.log("This is the formatted data ", formattedData)
   
         setChartData(formattedData);
       } catch (error) {
@@ -398,166 +341,6 @@ export default function PlayerDashboard() {
   
     fetchChartData();
   }, [user]);
-
-  // useEffect(() => {
-  //   // Function to fetch data from the checkin table
-  //   const fetchChartData = async () => {
-  //     try {
-  //       // Fetch the past week of data for wellness_id 1
-  //       const { data: wellness1Data, error: wellness1Error } = await supabase
-  //         .from("checkin")
-  //         .select("date, value")
-  //         .eq("wellness_id", 1)
-  //         .gte("date", new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString()) // Get data for the past week
-  //         .order("date");
-
-  //       const { data: wellness2Data, error: wellness2Error } = await supabase
-  //         .from("checkin")
-  //         .select("date, value")
-  //         .eq("wellness_id", 2)
-  //         .gte("date", new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString()) // Get data for the past week
-  //         .order("date");  
-
-  //       const { data: wellness3Data, error: wellness3Error } = await supabase
-  //         .from("checkin")
-  //         .select("date, value")
-  //         .eq("wellness_id", 3)
-  //         .gte("date", new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString()) // Get data for the past week
-  //         .order("date");   
-        
-  //       const { data: wellness4Data, error: wellness4Error } = await supabase
-  //         .from("checkin")
-  //         .select("date, value")
-  //         .eq("wellness_id", 4)
-  //         .gte("date", new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString()) // Get data for the past week
-  //         .order("date");   
-
-  //       const { data: wellness5Data, error: wellness5Error } = await supabase
-  //         .from("checkin")
-  //         .select("date, value")
-  //         .eq("wellness_id", 5)
-  //         .gte("date", new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString()) // Get data for the past week
-  //         .order("date");     
-
-  //       if (wellness1Error || wellness2Error || wellness3Error || wellness4Error || wellness5Error) {
-  //         throw wellness1Error || wellness2Error || wellness3Error || wellness4Error || wellness5Error;
-  //       }
-
-  //       // Format the fetched data for the chart
-  //         const formattedData = {
-  //           labels: [],
-  //           datasets: [
-  //             {
-  //               label: "Wellness Data 1 - Water",
-  //               data: [],
-  //               fill: false,
-  //               borderColor: "rgb(75, 192, 192)",
-  //               tension: 0.1,
-  //             },
-  //             {
-  //               label: "Wellness Data 2 - Sleep",
-  //               data: [],
-  //               fill: false,
-  //               borderColor: "rgb(255, 99, 132)",
-  //               tension: 0.1,
-  //             },
-  //             {
-  //               label: "Wellness Data 3 - Stress",
-  //               data: [],
-  //               fill: false,
-  //               borderColor: "rgb(54, 162, 235)",
-  //               tension: 0.1,
-  //             },
-  //             {
-  //               label: "Wellness Data 4 - Soreness",
-  //               data: [],
-  //               fill: false,
-  //               borderColor: "rgb(255, 205, 86)",
-  //               tension: 0.1,
-  //             },
-  //             {
-  //               label: "Wellness Data 5 - Energy",
-  //               data: [],
-  //               fill: false,
-  //               borderColor: "rgb(153, 102, 255)",
-  //               tension: 0.1,
-  //             },
-  //           ],
-  //         };
-
-  //       // Generate labels for the last 7 days
-  //       const currentDate = new Date();
-  //       for (let i = 6; i >= 0; i--) {
-  //         const date = new Date(currentDate);
-  //         date.setDate(currentDate.getDate() - i);
-  //         formattedData.labels.push(date.toISOString().split("T")[0]); // Add date to labels array
-  //       }
-
-  //       // Populate the data array
-  //       for (let i = 0; i < 7; i++) {
-  //         const currentDate = formattedData.labels[i];
-  //         const wellness1Entry = wellness1Data.find((entry) => entry.date === currentDate);
-  //         const wellness2Entry = wellness2Data.find((entry) => entry.date === currentDate);
-  //         const wellness3Entry = wellness3Data.find((entry) => entry.date === currentDate);
-  //         const wellness4Entry = wellness4Data.find((entry) => entry.date === currentDate);
-  //         const wellness5Entry = wellness5Data.find((entry) => entry.date === currentDate);
-  //         formattedData.datasets[0].data.push(wellness1Entry ? wellness1Entry.value : null);
-  //         formattedData.datasets[1].data.push(wellness2Entry ? wellness2Entry.value : null);
-  //         formattedData.datasets[2].data.push(wellness3Entry ? wellness3Entry.value : null);
-  //         formattedData.datasets[3].data.push(wellness4Entry ? wellness4Entry.value : null);
-  //         formattedData.datasets[4].data.push(wellness5Entry ? wellness5Entry.value : null);
-  //       }
-
-  //       // Set the formatted data to the state
-  //       setChartData(formattedData);
-  //     } catch (error) {
-  //       console.error("Error fetching checkin data:", error.message);
-  //     }
-  //   };
-
-  //   fetchChartData(); // Call the function to fetch checkin data
-  // }, []);
-
-  //binary data for multiplelinechart
-  // const checkinBinaryData = [];
-  // if (checkinData !== null) {
-  //   for (let i = 6; i >= 0; i--) {
-  //     const date = new Date(today);
-  //     date.setDate(today.getDate() - i);
-  //     const checkinEntry = checkinData.find(entry => {
-  //       const entryDate = new Date(entry.date);
-  //       return entryDate.toDateString() === date.toDateString();
-  //     });
-  //     checkinBinaryData.push(checkinEntry ? 1 : 0);
-  //   }
-  // }
-
-  // console.log("binary data", checkinBinaryData)
-
-  //end binary data
-
-  //test new chart
-  // const chartData = {
-  //   labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-  //   datasets: [
-  //     {
-  //       label: "Sample Data 1",
-  //       data: [12, 19, 3, 5, 2, 3, 15], // Sample data values for the first line
-  //       fill: false,
-  //       borderColor: "rgb(75, 192, 192)",
-  //       tension: 0.1,
-  //     },
-  //     {
-  //       label: "Sample Data 2", // Label for the second line
-  //       data: [5, 12, 7, 10, 4, 8, 11], // Sample data values for the second line
-  //       fill: false,
-  //       borderColor: "rgb(255, 99, 132)", // Different color for the second line
-  //       tension: 0.1,
-  //     },
-  //   ],
-  // };
-
-  // end test chart
 
   return (
     <DashboardLayout>
@@ -667,20 +450,8 @@ export default function PlayerDashboard() {
         <MDBox mt={4.5} mb={9}>
           <Grid container spacing={3}>
             <Grid item xs={12} md={12} lg={12}>
-              <MDBox mb={3}>
-                {/* <MultipleLineChart
-                color="success"
-                title="Check-in Results"
-                description="Days from the past week that you completed your check in."
-                date="Updated Today"
-                chart={{
-                  labels: labels,
-                  datasets: {
-                    label: "Completed Daily Wellness?",
-                    data: checkinBinaryData,//checkinData ? checkinData.map(entry => entry.value) : [],
-                  }
-                }}
-              /> */}
+              <MDBox>
+              <h3>Check-in data from the past week:</h3>
               </MDBox>
             </Grid>
           </Grid>
